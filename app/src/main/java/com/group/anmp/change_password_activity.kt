ef8dab1987e36e2baecb7f00ab905e972b2b4350
dastyns <s160420005@student.ubaya.ac.id>
2023-02-28 17:33:50 +0700

    tambah activity change password

---
+++ app/src/main/java/com/group/anmp/change_password_activity.kt
@@ -0,0 +1,13 @@
+package com.group.anmp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class change_password_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+
+        //change password
+    }
+}
